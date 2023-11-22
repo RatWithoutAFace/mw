2d63c78100fc7f053a0f21f2c490e4859c34e10e
RatWithAFace <126254038+RatWithoutAFace@users.noreply.github.com>
2023-11-22 10:13:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://movie-web.thec0mpu1er.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTZiZjBkMjc2NTgxYTIwOGFkNzBhMGUwMzVmOGQ2NyIsInN1YiI6IjY1NWUxNzExZDM5OWU2MDBlYzQzZTFhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jz2kPUUHcUWqcM2D6uwfYjjOuoXoBQvaSFC0mlJ7Das"
 };
